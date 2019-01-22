4704862ea5b12e944457a35f7a8101391fcc2982
Fixer <fixer3838@live.fr>
2019-01-22 23:30:44 +0100

    Added links to stylesheets + navbar + include slideshow and login

--- index.php
+++ index.php
@@ -1,3 +1,49 @@
 <?php 
-echo "index";
+session_start();
 ?>
+
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Inscription</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="./css/registration.css">
+    <link rel="stylesheet" type="text/css" href="./css/login-modal.css">
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <style>
+      .mySlides {display: None}
+      .w3-left, .w3-right, .w3-badge {cursor:pointer}
+      .w3-badge {height:13px;width:13px;padding:0} 
+    </style>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  </head>
+  <body>
+
+  <!-- Barre de naviguation -->
+  <nav class="navbar navbar-inverse">
+    <div class="container-fluid">
+      <ul class="nav navbar-nav navbar-center">
+        <a class="navbar-brand" href="#">RGraphy</a>
+      </ul>
+      </div>
+      <ul class="nav navbar-nav">
+        <li><a href="../index.php">Home</a></li>
+        <li><a href="./produits.php">Produits</a></li>
+        <li><a href="./contact.php">Contact</a></li>
+      </ul>
+      <ul class="nav navbar-nav navbar-right">
+        <li class="active"><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+        <li><a id="login-btn"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+      </ul>
+    </div>  
+  </nav>
+
+
+
+  
+  <?php include('./html/slideshow.php');?>
+  <?php include('./html/login.php');?>
+  </body>
+</html>
