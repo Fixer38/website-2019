726824c68ac91c0be96b8c8960b57b61af468992
Fixer <fixer3838@live.fr>
2019-01-23 23:37:00 +0100

    Added second slideshow for text beside the other one. + Javascript function to handle it.

--- html/slideshow.php
+++ html/slideshow.php
@@ -8,6 +8,8 @@
     <link rel="stylesheet" type="text/css" href="./css/login-modal.css">
     <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
     <style>
+      .right-content {margin: 0; position: absolute; top: 50%;-ms-transform: translateY(-50%);transform: translateY(-50%);left: 50%}
+      .left-content {margin: 0; position: absolute; top: 50%; -ms-transform: translateY(-50%);transform: translateY(-50%);right: 75%}
       .mySlides {display: None}
       .w3-left, .w3-right, .w3-badge {cursor:pointer}
       .w3-badge {height:13px;width:13px;padding:0} 
@@ -17,7 +19,7 @@
   </head>
   <body>
 
-<div class="w3-content w3-display-container" style="max-width:800px">
+<div class="w3-content w3-display-container right-content" style="max-width:50%">
   <img class="mySlides" src="./images/registration-background.jpg" style="width:100%">
   <img class="mySlides" src="./images/registration-background.jpg" style="width:100%">
   <img class="mySlides" src="./images/registration-background.jpg" style="width:100%">
@@ -27,24 +29,52 @@
     <span class="w3-badge demo w3-border w3-transparent w3-hover-white" onclick="currentDiv(1)"></span>
     <span class="w3-badge demo w3-border w3-transparent w3-hover-white" onclick="currentDiv(2)"></span>
     <span class="w3-badge demo w3-border w3-transparent w3-hover-white" onclick="currentDiv(3)"></span>
+    <span class="w3-badge demo w3-border w3-transparent w3-hover-white" onclick="currentDiv(4)"></span>
+    <span class="w3-badge demo w3-border w3-transparent w3-hover-white" onclick="currentDiv(5)"></span>
+  </div>
+</div>
+
+<div class="w3-content w3-display-container left-content" style="max-width:50%">
+  <div class="mySlides2"style="width:100%"><h1>There is some text</h1></div>
+  <div class="mySlides2"style="width:100%"><h1>There is some text2</h1></div>
+  <div class="mySlides2"style="width:100%"><h1>There is some text3</h1></div>
+  <div class="w3-center w3-container w3-section w3-large w3-text-white w3-display-bottommiddle" style="width:100%">
   </div>
 </div>
 
 <script>
 var slideIndex = 1;
 showDivs(slideIndex);
+showDivs2(slideIndex);
 
 function plusDivs(n) {
   showDivs(slideIndex += n);
+  showDivs2(slideIndex);
 }
 
 function currentDiv(n) {
   showDivs(slideIndex = n);
+  showDivs2(slideIndex);
 }
 
 function showDivs(n) {
   var i;
   var x = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("demo");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" w3-white", "");
+  }
+  x[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " w3-white";
+}
+function showDivs2(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides2");
   var dots = document.getElementsByClassName("demo");
   if (n > x.length) {slideIndex = 1}
   if (n < 1) {slideIndex = x.length}
