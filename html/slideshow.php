af1fd3ae40b0fae6bdd45b6f9096ff47df631985
Fixer <fixer3838@live.fr>
2019-01-25 11:37:12 +0100

    Added 2 more lines of text

--- html/slideshow.php
+++ html/slideshow.php
@@ -38,6 +38,8 @@
   <div class="mySlides2"style="width:100%"><h1>There is some text</h1></div>
   <div class="mySlides2"style="width:100%"><h1>There is some text2</h1></div>
   <div class="mySlides2"style="width:100%"><h1>There is some text3</h1></div>
+  <div class="mySlides2"style="width:100%"><h1>There is some text2</h1></div>
+  <div class="mySlides2"style="width:100%"><h1>There is some text3</h1></div>
   <div class="w3-center w3-container w3-section w3-large w3-text-white w3-display-bottommiddle" style="width:100%">
   </div>
 </div>
