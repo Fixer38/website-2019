f20328e2e496015bdcd3c50010eea715aa3f6da6
Fixer <fixer3838@live.fr>
2019-01-26 20:05:04 +0100

    Sql script to insert product informations into the produit table

---
+++ sql/insert_produit_values.sql
@@ -0,0 +1,6 @@
+insert into produit values(null, "Goutte d'eau", "raindrops.jpg", 24.99, 10);
+insert into produit values(null, "Groseilles rouges", "red_berries.png", 14.99, 10);
+insert into produit values(null, "Tunnel urbain", "tunnel.png", 34.99, 10);
+insert into produit values(null, "Fleur blanche", "white_flower.png", 24.99, 10);
+insert into produit values(null, "Ciel majestique", "sky.png", 29.99, 10);
+
