b0f6c3052fe76869777c87baded33797e5b9db6b
Fixer <fixer3838@live.fr>
2019-01-30 22:24:25 +0100

    Added second slide at the left to display info about the product and added "acheter" form

--- html/slideshow.php
+++ html/slideshow.php
@@ -1,6 +1,5 @@
-<?php
-session_start();
-?>
+<?php session_start(); 
+include('./php/products_info.php');?>
 <!DOCTYPE html>
 <html lang="fr" dir="ltr">
   <head>
@@ -11,92 +10,58 @@ session_start();
     <link rel="stylesheet" type="text/css" href="./css/login-modal.css">
     <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
     <style>
-      .w3-content {position: absolute;
-      max-width: 1200px;
-      height: 650px;
-      width: 1200px;
-      margin: -100px 0 0 -200px;
-      top: 20%;
-      left: 30%;}
-      .mySlides {display: None; width: 100%; height: 100%; right:50%}
+      .right-content {margin: 0; position: absolute; top: 50%;-ms-transform: translateY(-50%);transform: translateY(-50%);left: 50%}
+      .left-content {margin: 0; position: absolute; top: 50%; -ms-transform: translateY(-50%);transform: translateY(-50%);right: 75%}
+      .mySlides {display: None}
       .w3-left, .w3-right, .w3-badge {cursor:pointer}
       .w3-badge {height:13px;width:13px;padding:0} 
-      .product-info {top: 850px; height: 400px;}
-      .get-hidden {display: none;}
-      .activee {height: 100%; width: 100%;}
     </style>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
   </head>
   <body>
-  
-  
-<?php 
-include('./secret/mdp.php');
-try
-	{$bdd = new PDO("mysql:host=$host;dbname=$db", $user, $userpswd);}
-
-catch(Exception $e)
-    {die('Erreur : '.$e->getMessage());}  // arrêt en cas d'erreur
-
-// Requete permettant de récupérer toutes les informations sur le produit
-$products_info = $bdd->prepare("select * from produit");
-$products_info->execute();
-$products_info = $products_info->fetchAll();
+<div class="w3-content w3-display-container right-content" style="max-width:50%">
+<?php
+for($i = 0; $i < sizeof($_SESSION['images']); $i++) {
+  echo "<img class='mySlides' src='./images/{$_SESSION['images'][$i]}' style='width:100%'>";
+}
 ?>
-
-<!-- Slideshow pour images produit -->
-
-<div class="w3-content w3-display-container ">
-<?php 
-// php pour récupérer les images à afficher dans le slideshow
-foreach($products_info as $row) {
-  $image = $row['image'];
-  echo "<img class='mySlides' src='./images/{$image}'>"; 
-}?>
-<div class='w3-center w3-container w3-section w3-large w3-text-white w3-display-bottommiddle' style='width:100%'>
-  <div class='w3-left w3-hover-text-khaki' onclick='plusDiv-white' onclick='currentDiv(1)'></span>
-  <span class='w3-badge demo w3-border w3-transparent w3-hovers(-1)'>&#10094;</div>
-  <div class='w3-right w3-hover-text-khaki' onclick='plusDivs(1)'>&#10095;</div>
-  <span class='w3-badge demo w3-border w3-transparent w3-hover-white' onclick='currentDiv(2)'></span>
-  <span class='w3-badge demo w3-border w3-transparent w3-hover-white' onclick='currentDiv(3)'></span>
-  <span class='w3-badge demo w3-border w3-transparent w3-hover-white' onclick='currentDiv(4)'></span>
-  <span class='w3-badge demo w3-border w3-transparent w3-hover-white' onclick='currentDiv(5)'></span>
+  <div class="w3-center w3-container w3-section w3-large w3-text-white w3-display-bottommiddle" style="width:100%">
+    <div class="w3-left w3-hover-text-khaki" onclick="plusDivs(-1)">&#10094;</div>
+    <div class="w3-right w3-hover-text-khaki" onclick="plusDivs(1)">&#10095;</div>
+    <span class="w3-badge demo w3-border w3-transparent w3-hover-white" onclick="currentDiv(1)"></span>
+    <span class="w3-badge demo w3-border w3-transparent w3-hover-white" onclick="currentDiv(2)"></span>
+    <span class="w3-badge demo w3-border w3-transparent w3-hover-white" onclick="currentDiv(3)"></span>
+    <span class="w3-badge demo w3-border w3-transparent w3-hover-white" onclick="currentDiv(4)"></span>
+    <span class="w3-badge demo w3-border w3-transparent w3-hover-white" onclick="currentDiv(5)"></span>
+  </div>
 </div>
+<div class="w3-content w3-display-container left-content" style="max-width:50%">
+<?php
+for($i = 0; $i <sizeof($_SESSION['images']); $i++) {
+  echo "<div class='mySlides2' style='width:100%'><h1>Prix: {$_SESSION['prices'][$i]}</h1>";
+  echo "<h1>Stock: {$_SESSION['stocks'][$i]}</h1>";
+  echo "<h1>Description: {$_SESSION['descriptions'][$i]}</h1></br>";
+  echo "<form action='../php/to_basket.php'>";
+  echo "Quantité: </br><input type='int' name='quantity{$i}'></br>";
+  echo "<input type='submit' value='Ajouter au panier'></form></div>";
+}
+?>
+  <div class="w3-center w3-container w3-section w3-large w3-text-white w3-display-bottommiddle" style="width:100%">
+  </div>
 </div>
-<!-- fin du slideshow -->
-
-<!-- ajout d'un bouton acheter qui fera apparaitre un nouveau sideshow avec les informations sur le produit -->
-<div class="w3-content show_hide product-info buy-container" style="left:200px">Acheter</div>
-<div class="w3-content product-info slidingDiv">
-  <p>Here is some text</p>
-</div>
-
-
-
-<!-- script permettant de gérer le slideshow et le text qui slide-->
 <script>
-// function pour le slideshow apparaissant sur l'écran
-  $(".slidingDiv").hide();
-
-  $('.show_hide').click(function() {
-    $(this).next(".slidingDiv").slideToggle();
-    
- });
-// fin de la function pour le slideshow apparaissant à l'écran
-
-// functions pour le slideshow
 var slideIndex = 1;
 showDivs(slideIndex);
-
+showDivs2(slideIndex);
 function plusDivs(n) {
   showDivs(slideIndex += n);
+  showDivs2(slideIndex);
 }
-
 function currentDiv(n) {
   showDivs(slideIndex = n);
+  showDivs2(slideIndex);
 }
-
 function showDivs(n) {
   var i;
   var x = document.getElementsByClassName("mySlides");
@@ -112,8 +77,21 @@ function showDivs(n) {
   x[slideIndex-1].style.display = "block";  
   dots[slideIndex-1].className += " w3-white";
 }
-// fin de la fonction pour le slideshow
+function showDivs2(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides2");
+  var dots = document.getElementsByClassName("demo");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" w3-white", "");
+  }
+  x[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " w3-white";
+}
 </script>
-
 </body>
 </html>
