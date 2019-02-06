5402d93da8565c2f038ef8190ac23a4312a734c8
Fixer <fixer3838@live.fr>
2019-02-06 11:54:41 +0100

    Changed form method to be able to know which article got bought

--- html/slideshow.php
+++ html/slideshow.php
@@ -42,10 +42,12 @@ for($i = 0; $i <sizeof($_SESSION['images']); $i++) {
   echo "<div class='mySlides2' style='width:100%'><h1>Prix: {$_SESSION['prices'][$i]}</h1>";
   echo "<h1>Stock: {$_SESSION['stocks'][$i]}</h1>";
   echo "<h1>Description: {$_SESSION['descriptions'][$i]}</h1></br>";
-  echo "<form action='../php/to_basket.php'>";
-  echo "Quantité: </br><input type='int' name='quantity{$i}'></br>";
+  echo "<form action='./php/to_basket.php' method='post'>";
+  $buttoname = "quantity{$i}";
+  echo "Quantité: </br><input type='int' name=$buttoname></br>";
   echo "<input type='submit' value='Ajouter au panier'></form></div>";
 }
+  
 ?>
   <div class="w3-center w3-container w3-section w3-large w3-text-white w3-display-bottommiddle" style="width:100%">
   </div>
@@ -71,7 +73,7 @@ function showDivs(n) {
   for (i = 0; i < x.length; i++) {
     x[i].style.display = "none";  
   }
-  for (i = 0; i < dots.length; i++) {
+  for (i = 0; i < dots.length; i++) { 
     dots[i].className = dots[i].className.replace(" w3-white", "");
   }
   x[slideIndex-1].style.display = "block";  
