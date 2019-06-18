14a2dfd2bdf81fb972479341a1a8a7c0972dbcdd
Fixer <fixer3838@live.fr>
2019-06-18 22:50:51 +0200

    Sql script to add the foreign key to the commandes and lignescom tables

--- sql/add_foreign_key_ref_cli.sql
+++ sql/add_foreign_key_ref_cli.sql
@@ -1,5 +1,5 @@
 alter table commande 
-add column ref_cli int unsigned not null after date_com
+add column ref_cli int unsigned not null after date_com,
 add foreign key fk_ref_cli(ref_cli)
 references client(id)
 on delete no action
