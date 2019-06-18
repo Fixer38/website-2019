b94088c5a0bca6bac3c3c0629d2f146dbfc0394a
Fixer <fixer3838@live.fr>
2019-06-18 22:44:14 +0200

    Changed the view for the basket

--- html/basket.php
+++ html/basket.php
@@ -9,9 +9,12 @@ session_start();
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <link rel="stylesheet" type="text/css" href="../css/registration.css">
     <link rel="stylesheet" type="text/css" href="../css/login-modal.css">
+    <link rel="stylesheet" type="text/css" href="../css/mountain-background.css">
     <link rel="stylesheet" type="text/css" href="../css/basket.css">
     <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">    
     <style>
+      .absolute {font-family: 'Roboto'}
     </style>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
@@ -20,32 +23,34 @@ session_start();
 <?php include('./navbar.php'); ?>
 
 <?php
-echo "<div class='textbox' style='clear: both;'>";
-echo "<p class='alignleft'>".$_SESSION['street']." ".$_SESSION['hnumber']."</p>";
-echo "<p class='alignright'>".$_SESSION['localite']." ".$_SESSION['cp']."</p></div>";
 if (!empty($_SESSION['panier']['codeproduit'])) {
-  echo  "<div class='absolute'>";
-  echo "<div class='grid-container'>";
+  echo "<div class='grid-container' style='font-family: Roboto;'>";
+  echo "<div class='names'>".$_SESSION['name']." ".$_SESSION['fname']."</div>";
+  echo "<div class='street_hnumber'>".$_SESSION['street']." ".$_SESSION['hnumber']."</div>";
+  echo "<div class='gap'></div>";
+  echo "<div class='localite_cp'>".$_SESSION['localite']." ".$_SESSION['cp']."</div>";
   for($i=0;$i<sizeof($_SESSION['panier']['codeproduit']);$i++) {
     $product_number = $_SESSION['panier']['codeproduit'][$i]-1;
     $remove = "remove{$i}";
-    echo "<div><form action='../php/removeitem.php' method='post'><input type='submit' value='x' name=$remove class='btn-minus'></form></div>";
-    echo "<div><img class='resize' src='../images/{$_SESSION['images'][$product_number]}'></div>";
-    echo "<div>{$_SESSION['descriptions'][$product_number]}</div>";
+    echo "<div class='del_button'><form action='../php/removeitem.php' method='post'><input type='submit' value='x' name=$remove class='btn-minus'></form></div>";
+    echo "<div class'prod_image'><img class='resize' src='../images/{$_SESSION['images'][$product_number]}'></div>";
+    echo "<div class='prod_desc'>{$_SESSION['descriptions'][$product_number]}</div>";
     $add = "add{$i}";
     $minus = "minus{$i}";
-    echo "<div><form action='../php/incrementquant.php' method='post'><input type='submit' value='-' name=$minus class='btn-minus'><input type='submit' value='+' name=$add class='btn-plus'></form></div>";
-    echo "<div>{$_SESSION['panier']['quantite'][$i]}</div>";
-    $total += $_SESSION['prices'][$i] * $_SESSION['panier']['quantite'][$i];
-    $current = $_SESSION['prices'][$i] * $_SESSION['panier']['quantite'][$i];
-    echo "<div>{$current}</div>";
+    echo "<div class='quant_buttons'><form action='../php/incrementquant.php' method='post'><input type='submit' value='-' name=$minus class='btn-minus'><input type='submit' value='+' name=$add class='btn-plus'></form></div>";
+    echo "<div class='quant'>{$_SESSION['panier']['quantite'][$i]}</div>";
+    $total += $_SESSION['prices'][$product_number] * $_SESSION['panier']['quantite'][$i];
+    $current = $_SESSION['prices'][$product_number] * $_SESSION['panier']['quantite'][$i];
+    echo "<div class='price'>{$current}</div>";
     if($i === sizeof($_SESSION['panier']['codeproduit'])-1) {
       echo "<div></div>";
       echo "<div></div>";
       echo "<div></div>";
       echo "<div></div>";
       echo "<div>Total</div>";
-      echo "<div>{$total}</div></div></div>";
+      echo "<div>{$total}</div>";
+      echo "<div class='val'><form action='../php/basket_validation.php' method='post'>
+      <input class='valid_button' type='submit' value='Valider le panier'/></form></div></div>";
     }
   } 
 }
@@ -54,3 +59,4 @@ else {
   echo "<div>Votre panier est vide</div>";
 }
 ?>
+</body> 
