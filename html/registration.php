c884cab0da1ca59e78e97ac1532f2b1f6dc36f72
Fixer <fixer3838@live.fr>
2019-06-18 22:46:38 +0200

    Added the links to the 2 css pages

--- html/registration.php
+++ html/registration.php
@@ -5,8 +5,10 @@
     <meta charset="utf-8">
     <title>Inscription</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
     <link rel="stylesheet" type="text/css" href="../css/registration.css">
     <link rel="stylesheet" type="text/css" href="../css/login-modal.css">
+    <link rel="stylesheet" type="text/css" href="../css/mountain-background.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 
